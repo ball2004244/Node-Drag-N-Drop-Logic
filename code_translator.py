c787028a65bdc5c7e8f05c82a22920e10b576e04
Tam Nguyen <nguyenminhtam7124@gmail.com>
2023-11-01 21:30:59 -0400

    Update code_translator

--- code_translator.py
+++ code_translator.py
@@ -10,38 +10,42 @@ class Translator:
         self.variables = {}
         self.keywords = {
             'print': 'print("%s")',
-            'if': 'if %s:',
-            'else': 'else:',
-            'for': 'for %s:',
-            'in': 'in %s',
-            'while': 'while %s:',
             'range': 'range(%s)',
         }
         
-        self.pseudo_keywords = {
-            'do': '%s',
+        self.if_keywords = {
+            'if': 'if %s:',
             'then': '%s',
             'condition': '%s',
-            'variable': '%s',
+            'else': 'else:\n%s%s' % (self.indentation, '%s'),
         }
         
-        self.code_block_keywords = {
-            'if',
-            'for',
-            'while'
+        self.for_keywords = {
+            'for': 'for %s:',
+            'variable': '%s ',
+            'in': 'in %s',
+            'do': '%s',
         }
         
-    def handle_if(self, raw_code: str) -> str:
-        pass
-    
-    def handle_for(self, raw_code: str) -> str:
-        pass
+        self.while_keywords = {
+            'while': 'while %s:',
+            'do': '%s',
+            'condition': '%s',
+        }
+        
+        # merge all keywords
+        self.all_keywords = {
+            **self.keywords,
+            **self.if_keywords,
+            **self.for_keywords,
+            **self.while_keywords,
+        }
 
     def convert(self) -> str:
         try:
             converted_code = ''
             for key, value in self.json_code.items():
-                if key not in self.keywords and key not in self.pseudo_keywords:
+                if key not in self.all_keywords:
                     raise Exception(f'Keyword {key} is not supported')
 
                 # start translating
@@ -60,40 +64,37 @@ class Translator:
 
             return ''
 
-    
     def format_keyword(self, keyword: str, value: Union[str, dict, None]) -> str:
         formatted_value = self.format_value(keyword, value)
-        
         # Return code block directly from format_value function
         if self.is_code_block(value):
             return str(formatted_value)
 
         if keyword in self.keywords:
-            return self.keywords[keyword] % str(formatted_value)
+            return self.handle_basic(keyword, formatted_value)
         
-        if keyword in self.pseudo_keywords:
-            return self.pseudo_keywords[keyword] % str(formatted_value)
-
+        # return the same keyword if not in keywords
         return keyword
     
-    def format_value(self, key: str, value: Union[str, dict, None]) -> str:
+    def format_value(self, key: str, value: Union[str, dict, None]) -> Union[str, dict, None]:
         if not self.is_code_block(value):
-            return value
-        
-        code_block = f'{key} - This is a code-block'
+            return value        
+        code_block = f''
         
         #TODO: Implement if-else
+        code_block += self.handle_if(key, value)
 
         #TODO: Implement for loop
-        
-        #TODO: Implement while loop
-        
-        
-        #TODO: Implement def
-        
-        #TODO: Implement class
+        code_block += self.handle_for(key, value)
 
-        return code_block
+        #TODO: Implement while loop
+        code_block += self.handle_while(key, value)
+        
+        #TODO: Implement def later
+        
+        #TODO: Implement class later
+
+        return value
 
     
     def is_code_block(self, code: Union[str, dict, None]) -> bool:
@@ -101,6 +102,72 @@ class Translator:
             return True
         return False
     
+    def handle_basic(self, key: str, raw_code: Union[str, dict, None]) -> str:
+        if key in self.keywords:
+            return self.keywords[key] % raw_code
+        
+        return ''
+
+    def handle_if(self, key: str, raw_code: Union[str, dict, None]) -> str:
+        if key != 'if':
+            return ''
+
+        output = ''
+
+        # get the condition
+        condition = self.if_keywords['condition'] % raw_code['condition']
+        if_code = self.if_keywords['if'] % condition
+        then_code = self.if_keywords['then'] % raw_code['then']
+        else_code = ''
+
+        if 'else' in raw_code:
+            else_code = self.if_keywords['else'] % raw_code['else']
+        
+        # format the condition
+        # add to output
+        output += f'{if_code}\n'
+        output += f'{self.indentation}{then_code}\n'
+        output += f'{else_code}\n'
+
+        return output
+
+    def handle_for(self, key: str, raw_code: Union[str, dict, None]) -> str:
+        if key != 'for':
+            return ''
+
+        output = ''
+
+        # get the condition
+        condition = self.for_keywords['variable'] % raw_code['variable']
+        in_code = self.for_keywords['in'] % raw_code['in']
+        do_code = self.for_keywords['do'] % raw_code['do']
+        for_code = self.for_keywords['for'] % (condition + in_code)
+
+        # format the condition
+        # add to output
+        output += f'{for_code}\n'
+        output += f'{self.indentation}{do_code}\n'
+
+        return output
+
+    def handle_while(self, key: str, raw_code: Union[str, dict, None]) -> str:
+        if key != 'while':
+            return ''
+
+        output = ''
+
+        # get the condition
+        condition = self.while_keywords['condition'] % raw_code['condition']
+        while_code = self.while_keywords['while'] % condition
+        do_code = self.while_keywords['do'] % raw_code['do']
+
+        # format the condition
+        # add to output
+        output += f'{while_code}\n'
+        output += f'{self.indentation}{do_code}\n'
+
+        return output
+
 if __name__ == '__main__':
     code = ''
     filename = input('Enter filename: ')
@@ -111,4 +178,4 @@ if __name__ == '__main__':
     translator = Translator(code)
     translated_code = translator.convert()
     
-    print(translated_code)
+    # print(translated_code)
